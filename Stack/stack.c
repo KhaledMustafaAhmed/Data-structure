deba3c80b1062b7d637ca2fd31649abba72c6ca7
Khaled M. Abdelkader <m.abdelkader.khaled@gmail.com>
2024-04-04 17:31:47 +0200

    Stack- linkedList based

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-/* stack based on array */
+#include <stdlib.h>
+/******************************************************************/
+/*                     stack based on array                       */
+/******************************************************************/
+
+#define StackEntry   int
 #define MAXSTACK    100
+typedef struct stacknode{
+    StackEntry entry;
+    struct stacknode *next;
+}StackNode_t;
+
+typedef struct stack{
+    StackNode_t *top;
+    int size;
+}Stack_t;
+
+#if 0
 
 typedef struct stack{
 int top;
-int stackElements[MAXSTACK];
 int size;
+StackEntry stackElements[MAXSTACK];
 }Stack_t;
+
 /* 
 for example: 
 void main()
@@ -28,7 +45,7 @@ pre: the stack is initialized and not full
 post: the element is has been stored at the top of the stack and e does not 
 change.
 */
-void PushStack(int element ,Stack_t * stackptr)
+void PushStack(StackEntry element ,Stack_t * stackptr)
 {
     stackptr->stackElements[stackptr->top++] = element;
     stackptr->size++;
@@ -54,7 +71,7 @@ int StackEmpty(Stack_t * stackptr)
 pre: stack is initialized and not empty
 post: pop the last element from stack and decrement the stack size, top.
 */
-void PopStack(int *pe ,Stack_t * stackptr)
+void PopStack(StackEntry *pe ,Stack_t * stackptr)
 {
     stackptr->top--;
     *pe = stackptr->stackElements[stackptr->top];
@@ -64,7 +81,7 @@ void PopStack(int *pe ,Stack_t * stackptr)
 pre: stack is initialized and not empty
 post: talk a copy of the last element from stack .
 */
-void StackTop(int *pe ,Stack_t * stackptr)
+void StackTop(StackEntry *pe ,Stack_t * stackptr)
 {
     *pe = stackptr->stackElements[stackptr->top-1];
 }
@@ -74,3 +91,74 @@ void ClearStack(Stack_t * stackptr)
     stackptr->top = 0;
     stackptr->size = 0;
 }
+void TraverseStack(Stack_t * stackptr, void(*ptrFunc)(StackEntry e))
+{
+    int i;
+    for(i= stackptr->top; i>0; i--)
+    {
+        (*ptrFunc)(stackptr->stackElements[i-1]);
+    }
+}
+void StackSize(Stack_t * stackptr, int *size)
+{
+    *size = stackptr->size;
+}
+#endif
+/******************************************************************/
+/*                   stack based on Linked List                   */
+/******************************************************************/
+#if 1
+/* Stack first not point to any stack node. */
+void CreateStack(Stack_t *stackptr)
+{
+    stackptr->top = NULL;
+    stackptr->size = 0;
+}
+void PushStack(Stack_t *stackptr, StackEntry e)
+{
+    StackNode_t *node = (StackNode_t *)malloc(sizeof(StackNode_t));
+    node->entry = e;
+    node->next = stackptr->top;
+    stackptr->top = node;
+    stackptr->size++;
+}
+void PopStack(Stack_t *stackptr, StackEntry *pe)
+{
+    *pe = stackptr->top->entry;
+    StackNode_t *ptr = stackptr->top;
+    stackptr->top = stackptr->top->next;
+    free(ptr);
+    ptr = NULL;
+    stackptr->size--;
+}
+void StactTop(Stack_t *stackptr, StackEntry *pe)
+{
+    *pe = stackptr->top->entry;
+}
+void ClearStack(Stack_t *stackptr)
+{
+    StackNode_t *pn = stackptr->top;
+    StackNode_t *qn = stackptr->top;
+    while(pn != NULL)
+    { 
+        pn = pn->next;
+        free(qn);
+        qn = pn;
+    }
+    stackptr->top = NULL;
+    stackptr->size = 0;
+}
+void TraverseStack(Stack_t * stackptr, void(*ptrFunc)(StackEntry e))
+{
+    StackNode_t *pn = stackptr->top;
+    while(pn)
+    {
+        (*ptrFunc)(pn->entry);
+        pn= pn->next;
+    }
+}
+void StackSize(Stack_t * stackptr, int *size)
+{
+    *size = stackptr->size;
+}
+#endif
--- Stack/stack.h
+++ Stack/stack.h
@@ -1,30 +1,69 @@
 /*  
-Stack data structure: known as LIFO or Last in first out.
-you can only puch a data from same data type.
-operations on satck: 
-1- push >> inset element from top
-2- pop >> remove the last inserted element
-3- peek / top >> tack the top element without remove it.
-4- is empty >> check if it is empty or not.
-5- is full >> check if it is full or not.
-to push data to stack you need to know the size of array so you need to allocate some memory for stack. 
-there is two ways of implementing stack >> 1- based arrays   2- based linked list
+"The stack data structure is known as LIFO, which stands for 
+Last In, First Out. 
+It allows you to push elements of the same data type onto it. 
+The operations that can be performed on a stack include:
+Push: Insert an element onto the top of the stack.
+Pop: Remove the last inserted element from the stack.
+Peek/Top: View the top element of the stack without removing it.
+Is Empty: Check if the stack is empty.
+Is Full: Check if the stack is full.
+When pushing data onto the stack, you need to know the size of 
+the array to allocate memory for the stack. There are two ways 
+to implement a stack: based on arrays or based on linked lists."
 */
+/******************************************************************/
+/*                     stack based on array                       */
+/******************************************************************/
 
-/* stack based on array */
+#if 0
+#define StackEntry   int
 #define MAXSTACK    100
-
 typedef struct stack{
 int top;
-int stackElements[MAXSTACK];
+StackEntry stackElements[MAXSTACK];
 int size;
 }Stack_t;
 
-/* this function to initialize the top to -1 */
 void CreateStack(Stack_t *stackptr);
-/* to push new element into stack */
-void PushStack(int element ,Stack_t * stackptr);
-/*1 if full, 0 otherwise*/
+void PushStack(StackEntry element ,Stack_t * stackptr);
 int StackFull(Stack_t * stackptr);
-/* pop element from stack */
-void PopStack(int *pe ,Stack_t * stackptr);
+int StackEmpty(Stack_t * stackptr);
+void PopStack(StackEntry *pe ,Stack_t * stackptr);
+void StackTop(StackEntry *pe ,Stack_t * stackptr);
+void ClearStack(Stack_t * stackptr);
+void TraverseStack(Stack_t * stackptr, void(*ptrFunc)(StackEntry e));
+void StackSize(Stack_t * stackptr, int *size);
+#endif
+/* "We have completed the APIs for the stack data structure using arrays. 
+However, allocating a maximum stack size of 100 elements when only half
+are typically used consumes unnecessary memory. To address this, 
+we are considering implementing the stack using a linked list. 
+This approach uses memory more efficiently by only allocating space 
+for elements as they are needed. However, it is important to note that 
+using a linked list may result in higher processing time compared to 
+an array-based implementation." */
+/******************************************************************/
+/*                   stack based on Linked List                   */
+/******************************************************************/
+#if 1
+#define StackEntry   int
+
+typedef struct stacknode{
+    StackEntry entry;
+    struct stacknode *next;
+}StackNode_t;
+
+typedef struct stack{
+    StackNode_t *top;
+}Stack_t;
+
+void CreateStack(Stack_t *stackptr);
+void PushStack(Stack_t *stackptr, StackEntry e);
+void PopStack(Stack_t *stackptr, StackEntry *pe);
+void StactTop(Stack_t *stackptr, StackEntry *pe);
+void ClearStack(Stack_t *stackptr);
+void TraverseStack(Stack_t * stackptr, void(*ptrFunc)(StackEntry e));
+void StackSize(Stack_t * stackptr, int *size);
+
+#endif
